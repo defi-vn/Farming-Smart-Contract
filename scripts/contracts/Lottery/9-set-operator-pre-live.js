3ae1f7982b97e6907f321befc25a199fe73093d4
HT <huytq@blockchaindeveloper.asia>
2021-12-30 10:14:09 +0700

    feat: Script to set operator roles for tester on pre-live version

---
+++ scripts/contracts/Lottery/9-set-operator-pre-live.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+const deployInfo = require("../../../deploy.json");
+
+const CONTRACT_NAME = "Lottery";
+const OPERATOR = [
+  "0xD08b70DbA72514Cba07852BF043551a5980f52E5",
+  "0x752F4d07DB335ae231014A74d23D98fcF99f7fe9"
+];
+
+async function setOperator() {
+  // Set operator
+  const [owner] = await hre.ethers.getSigners();
+  const factory = await hre.ethers.getContractFactory(CONTRACT_NAME);
+  await factory
+    .connect(owner)
+    .attach(deployInfo['pre-live'].Lottery)
+    .setOperators(OPERATOR, OPERATOR.map(operator => true));
+  console.log(`${OPERATOR} has been granted operator role`);
+}
+
+setOperator();
