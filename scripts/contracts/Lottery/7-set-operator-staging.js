e6dff1f698e93f6878ae83f0839782d2632e8095
HT <huytq@blockchaindeveloper.asia>
2021-12-31 09:50:37 +0700

    feat: Deploy Chainlink VRF version to staging and beta environments

--- scripts/contracts/Lottery/7-set-operator-staging.js
+++ scripts/contracts/Lottery/7-set-operator-staging.js
@@ -4,7 +4,8 @@ const deployInfo = require("../../../deploy.json");
 const CONTRACT_NAME = "Lottery";
 const OPERATOR = [
   "0xD08b70DbA72514Cba07852BF043551a5980f52E5",
-  "0x752F4d07DB335ae231014A74d23D98fcF99f7fe9"
+  "0x752F4d07DB335ae231014A74d23D98fcF99f7fe9",
+  "0x96aD5880f6899E28ee8af2121DF83bb324A535C2"
 ];
 
 async function setOperator() {
